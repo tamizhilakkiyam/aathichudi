f55683ff2a7f07dd3d74fd5fbd5b765c3e591ac9
scientific-dev <69678617+Scientific-Guy@users.noreply.github.com>
2022-01-14 13:22:32 +0530

    feat: initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Footer from './Footer.svelte';
+</script>
+
+<div class="cover">
+    <h1>ஆத்திசூடி</h1>
+    <p>ஔவையாரின் ஆத்திசூடி / Avvaiyar's Aathichudi</p>
+</div>
+
+<div class="content" id="content">
+</div>
+
+<Footer/>
---
+++ src/Footer.svelte
@@ -0,0 +1,8 @@
+<div class="footer">
+    <h1 class="tamil">ஆத்திசூடி</h1>
+
+    <p class="tamil bold">தமிழிலக்கியம் © {new Date().getFullYear()}</p>
+    <p class="english bold">Tamizhilakkiyam © {new Date().getFullYear()}</p><br/>
+
+    <a class="english" href="https://github.com/tamizhilakkiyam/aathichudi">Github</a>
+</div>
